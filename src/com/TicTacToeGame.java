644c79f13442368424a465a28cd9f4b3f0555e05
b115030 <iitiangokul@gmail.com>
2020-09-26 10:59:24 +0530

    ADDED: showBoard branch

--- src/com/TicTacToeGame.java
+++ src/com/TicTacToeGame.java
@@ -7,18 +7,49 @@ public class TicTacToeGame {
 	/*useCase method creates char array of 10
 	 * and initializes it by null
 	 */
+	char[] board = new char[10];
 	public void createBoard() {
 		
-		char[] board = {'\0', '\0','\0','\0','\0','\0','\0','\0','\0','\0','\0' };
-        for (int i=1;i<=10;i++) {
+		char[] board = {'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+        for (int i = 1;i <= 9;i++) {
         	System.out.println(board[i]);
+        	this.board[i]=board[i];
         	
         }
         }
-	public static void main(String[] args) {
+        //UC2
+        public void letterChoice() {
+        	Scanner sc=new Scanner(System.in);
+        	System.out.println("Choose 1. X \n 2. O");
+        	if(sc.nextInt() == 1) {
+        		this.player ='X';
+        		this.comp ='O';
+        	}
+        	else {
+        		this.player ='O';
+        		this.comp ='X';
+        	}
+        	System.out.println("your choice "+player+" computer choice "+comp);
+        	
+        }
+        //UC3
+        public void showBoard() {
+        	System.out.println(board[1] + "|" + board[2] + "|" + board[3]);
+        	System.out.println("-------");
+        	System.out.println(board[4] + "|" + board[5] + "|"+ board[6]);
+        	System.out.println("-------");
+        	System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
+        	
+        }
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TicTacToeGame ttt=new TicTacToeGame();
-		ttt.createBoard();
+		
+		TicTacToeGame tictactoe=new TicTacToeGame();
+		
+		tictactoe.createBoard();
+		tictactoe.letterChoice();
+		tictactoe.showBoard();
+		
 	}
 
 }
